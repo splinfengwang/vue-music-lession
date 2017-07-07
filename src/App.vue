6bbcfbf6815d2d40bef2bb7020b4fcef4be98fc5
王林峰 <1175692055@qq.com>
2017-07-08 00:04:27 +0800

    Vue音乐播放器

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+
+export default {
+  name: 'app',
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
